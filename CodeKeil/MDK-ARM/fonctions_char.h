b94ea7c75f144a109736127712023dddb40221ee
DvAx <devaux25@gmail.com>
2019-03-19 16:41:09 +0100

    nouveau projet vide avec code module bluetooth

--- CodeKeil/MDK-ARM/fonctions_char.h
+++
@@ -1,5 +0,0 @@
-#include <stm32f4xx_hal.h>
-
-uint8_t tabsEquals(char* buffer, char* exp_anwser);
-void split_trame(char* trame, unsigned int size, char reponses[3][40]);
-char * myStrcat(char* commande, char* suiteCommande);
