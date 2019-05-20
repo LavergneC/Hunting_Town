41fadfebd9761259cd8b1f8d786e2ded211ba5b5
Nicolas <nicolas.gibaud7@gmail.com>
2019-05-20 18:24:56 +0200

    Affichage fenêtre de lancement pour les vidéos

--- CodeMap/UI_choix_videos.py
+++ CodeMap/UI_choix_videos.py
@@ -1,12 +1,15 @@
 from tkinter import *
+import tkinter.messagebox
 from ftplib import FTP
 import time
 from threading import Thread
 
 class Interface(Frame):
     
-    def __init__(self, fenetre, **kwargs):
-        Frame.__init__(self, fenetre, width=850, height=576, **kwargs)
+    def __init__(self, session_ftp, fenetre, **kwargs):
+        self.ftp = session_ftp
+        
+        Frame.__init__(self, fenetre, width=10000, height=5000, **kwargs)
         self.pack(fill = BOTH)
         
         self.message = Label(self, text = "Choix de la vidéo à lire ")
@@ -38,22 +41,28 @@ class Interface(Frame):
         self.case6.pack()
     
     def envoi_video(self):
+        tkinter.messagebox.showinfo("Info", "Vidéo lancée !")
+        
         with open("envoi_videos", 'w', encoding="utf-8") as fic_ordre:
             fic_ordre.write("{}".format(self.choix.get()))
-        print("Envoi de la vidéo {}".format(self.choix.get()))
 
+        with open("envoi_videos", 'rb') as fic_ordre: 
+            ftp.storbinary("STOR ordre_video", fic_ordre)
+
+        print("Envoi de la vidéo {}".format(self.choix.get()))
+        
 # FTP
 ftp = FTP("demo.wftpserver.com")
 ftp.login(user="demo-user", passwd="demo-user")
 ftp.cwd("upload")
 
 fenetre_principale = Tk()
-UI = Interface(fenetre_principale)
+UI = Interface(ftp, fenetre_principale)
 
 def get_coor_gps():
     ftp.retrbinary("RETR coor_gps", open("coor_gps", "wb").write)
-    fenetre_principale.after(5000, get_coor_gps)
+    fenetre_principale.after(7000, get_coor_gps)
 
-fenetre_principale.after(5000, get_coor_gps)
+fenetre_principale.after(7000, get_coor_gps)
 
 UI.mainloop()
